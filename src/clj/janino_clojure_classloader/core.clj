a7b08046226ca3599ef223ce9fe08aa62acbb6a4
Jonas Östlund <uppfinnarjonas@gmail.com>
2019-12-06 17:55:27 +0100

    good

---
+++ src/clj/janino_clojure_classloader/core.clj
@@ -0,0 +1,33 @@
+(ns janino-clojure-classloader.core
+  (:require [clojure.java.io :as io])
+  (:import [clojure.lang DynamicClassLoader]
+           [org.codehaus.janino JavaSourceClassLoader]
+           ;Mjao
+           ))
+
+(defn current-loader []
+  (.deref clojure.lang.Compiler/LOADER))
+
+
+(def janino-loader (JavaSourceClassLoader.
+                    nil
+                    (into-array [(io/file "janino")])
+                    nil))
+
+(defn set-source-loader []
+  (set! DynamicClassLoader/custom_loader janino-loader))
+
+(set-source-loader)
+
+
+
+(import 'Mjao)
+
+
+(defn load-mjao []
+  (.loadClass janino-loader "Mjao"))
+
+
+
+
+
