25711bcb3551bf4b513f799746b92ba63213ca97
Jonas Östlund <uppfinnarjonas@gmail.com>
2019-12-06 21:11:31 +0100

    this works great

--- janino/Mjao.java
+++ janino/Mjao.java
@@ -1,9 +1,8 @@
 import clojure.lang.PersistentVector;
 import java.util.ArrayList;
 
-
 public class Mjao {
     public static int magicOp(int x) {
-        return x + 1000;
+        return x + 10;
     }
 }
--- src/clj/janino_clojure_classloader/core.clj
+++ src/clj/janino_clojure_classloader/core.clj
@@ -1,41 +1,4 @@
 (ns janino-clojure-classloader.core
-  (:require [clojure.java.io :as io])
-  (:import [clojure.lang DynamicClassLoader]
-           [org.codehaus.janino JavaSourceClassLoader]
-           Mjao))
-#_(import 'clojure.lang.AFn)
+  (:import Mjao))
 
-;(set! clojure.lang.DynamicClassLoader/custom_loader nil)
-;(import 'Mjao)
-#_(println "majo value: " (Mjao/magicOp 3))
-
-(comment
-
-  
-  (defn current-loader []
-    (.deref clojure.lang.Compiler/LOADER))
-
-
-  (def janino-loader (JavaSourceClassLoader.
-                      nil
-                      (into-array [(io/file "janino")])
-                      nil))
-
-  
-  (defn set-source-loader []
-    (set! DynamicClassLoader/custom_loader janino-loader))
-
-
-  (set-source-loader)
-
-  (import 'Mjao)
-
-
-  (defn load-mjao []
-    (.loadClass janino-loader "Mjaoll"))
-
-
-
-
-
-  )
+(println "The result is " (Mjao/magicOp 3))
