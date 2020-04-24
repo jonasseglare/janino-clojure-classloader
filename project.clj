69cb25987756400e25463e9e75197cb977830515
Jonas Östlund <uppfinnarjonas@gmail.com>
2020-04-24 15:18:11 +0200

    move it

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :source-paths ["src/clj"]
+  :profiles {:repl {:source-paths ["src/clj"]}}
   :java-source-paths ["src/java"]
   :jvm-opts ["-Djanino_src_path=reloaded_java_sources"]
   :dependencies [[org.clojure/clojure "1.10.1"]
