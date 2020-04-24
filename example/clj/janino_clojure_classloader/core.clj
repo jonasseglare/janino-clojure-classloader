c6a60aef3ed95ccd3989f8f082fec00668478570
Jonas Östlund <uppfinnarjonas@gmail.com>
2020-04-24 15:29:20 +0200

    Complete working example

--- example/clj/janino_clojure_classloader/core.clj
+++ example/clj/janino_clojure_classloader/core.clj
@@ -9,11 +9,20 @@
             )
   (:import Mjao ADNumber))
 
+;; When we reload the current file, this should display the *current* value
+;; in the Mjao.java source code, always.
 (println "The result is " (Mjao/magicOp 3))
 
-(println "Same class" (= (class (other/get-instance))
-                         (class (Mjao.))))
+;; This checks that the same class is also reimported in the required namespace
+(println "Same class?" (if (= (class (other/get-instance))
+                              (class (Mjao.)))
+                         "YES (things look good :-) )"
+                         "NO (something is wrong)"))
 
+;; This function can be used to test that a function in another namespace
+;; will also reimport the same class definitions, so that there are no
+;; incompatibilities. Currently, that namespace is required with `:reload` so that
+;; it gets reloaded whenever we reload this namespace.
 (defn run-test []
   (other/add-ad (Mjao/getX)
                 (Mjao/getY)))
--- example/reloaded_java/Mjao.java
+++ example/reloaded_java/Mjao.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 
 public class Mjao {
     public static int magicOp(int x) {
-        return x + 1000;
+        return x + 100000;
     }
 
     public static ADNumber getX() {
