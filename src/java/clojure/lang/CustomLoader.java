b46e6fbf5115bcec8707406b7c2f07c9bddd503e
Jonas Östlund <uppfinnarjonas@gmail.com>
2020-04-24 13:11:26 +0200

    Here is one

---
+++ src/java/clojure/lang/CustomLoader.java
@@ -0,0 +1,46 @@
+package clojure.lang;
+import org.codehaus.janino.JavaSourceClassLoader;
+import java.io.File;
+import java.nio.file.FileSystems;
+import java.util.Date;
+
+public class CustomLoader {
+
+    public static File src_path = new File("janino");
+    public static java.lang.ClassLoader custom_loader = null;
+    public static long latest = 0;
+
+    public static void reset() {
+        custom_loader = null;
+    }
+
+    private static long getLatestModifiedDate(File dir) {
+        File[] files = dir.listFiles();
+        long latestDate = 0;
+        for (File file : files) {
+            long fileModifiedDate = file.isDirectory() 
+                ? getLatestModifiedDate(file) : file.lastModified();
+            if (fileModifiedDate > latestDate) {
+                latestDate = fileModifiedDate;
+            }
+        }
+        return Math.max(latestDate, dir.lastModified());
+    }
+    
+    public static java.lang.ClassLoader get() {
+        long x = getLatestModifiedDate(src_path);
+        if (x > latest) {
+            custom_loader = null;
+            latest = x;
+        }
+        
+        if (custom_loader == null) {
+            custom_loader = new JavaSourceClassLoader(
+                ClassLoader.getSystemClassLoader(),
+                new File[]{src_path}, 
+                null);
+        }
+        return custom_loader;
+    }
+}
+    
