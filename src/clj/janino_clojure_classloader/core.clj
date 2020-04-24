3431b1018b5ee88d1003709e998aca7a077fdaf9
Jonas Östlund <uppfinnarjonas@gmail.com>
2020-04-24 09:11:20 +0200

    wip

--- src/clj/janino_clojure_classloader/core.clj
+++ src/clj/janino_clojure_classloader/core.clj
@@ -1,4 +1,8 @@
 (ns janino-clojure-classloader.core
+  (:require [janino-clojure-classloader.other :as other])
   (:import Mjao))
 
 (println "The result is " (Mjao/magicOp 3))
+
+(println "Same class" (= (class (other/get-instance))
+                         (class (Mjao.))))
